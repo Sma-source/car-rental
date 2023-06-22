c65d737d0b666a9a7e07965fab59d82672d46319
Sma-source <rabhi.smain@gmail.com>
2023-06-22 18:36:37 +0200

    add property btnType

--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,6 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
+  btnType?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
