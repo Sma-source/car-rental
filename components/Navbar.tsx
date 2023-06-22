494ede7b7fc9e285b62af70eb10d23f789e98ba1
Sma-source <rabhi.smain@gmail.com>
2023-06-22 18:35:23 +0200

    update navbar: add custombutton

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import CustomButton from "./CustomButton";
 
 const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
-      <nav className="max-w-[144px] mx-auto flex justify-between items-center sm:px-16 px:6 py-4">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px:6 py-4">
         <Link href="/" className="flex justify-center items-center">
+          <span className="font-extrabold mx-2">CarRental</span>
           <Image
             src="/logo.svg"
             alt="logo"
@@ -15,6 +17,10 @@ const Navbar = () => {
             className="object-contain"
           />
         </Link>
+        <CustomButton
+          title="SignIn"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
       </nav>
     </header>
   );
