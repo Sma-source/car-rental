a2dc64820b175d180bd26bbd45baadaf48515f50
Sma-source <rabhi.smain@gmail.com>
2023-06-27 18:53:24 +0200

    update ui

--- components/SearchManufacture.tsx
+++ components/SearchManufacture.tsx
@@ -1,6 +1,10 @@
+import { SearchManufactureProps } from "@/types";
 import React from "react";
 
-const SearchManufacture = () => {
+const SearchManufacture = ({
+  manufacture,
+  setManufacture,
+}: SearchManufactureProps) => {
   return <div>SearchManufacture</div>;
 };
 
