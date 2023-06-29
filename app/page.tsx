19e13b29c75df0f15d745a18f98684f39465f32a
Sma-source <rabhi.smain@gmail.com>
2023-06-29 17:52:26 +0200

    display data and render card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import { Hero, SearchBar, CustomFilter } from "@/components";
+import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  // check if data empty
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -17,6 +23,20 @@ export default function Home() {
             <CustomFilter />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
