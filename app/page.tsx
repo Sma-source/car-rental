50d822881febbc0419afa12e848ad36e7a810630
Sma-source <rabhi.smain@gmail.com>
2023-06-29 19:51:30 +0200

    modify import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import { Hero, SearchBar, CustomFilter } from "@/components";
-import CarCard from "@/components/CarCard";
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
