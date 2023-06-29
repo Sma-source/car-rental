3f9e4e7f96c43b125774159dd34bea19e33c4486
Sma-source <rabhi.smain@gmail.com>
2023-06-29 17:04:29 +0200

    importr customfilter

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, SearchBar } from "@/components";
+import { Hero, SearchBar, CustomFilter } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,11 +7,15 @@ export default function Home() {
       <Hero />
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4xl">Car Catalogue</h1>
-          <p>Explore the cars you might like</p>
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
         <div className="home__filters">
           <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter />
+            <CustomFilter />
+          </div>
         </div>
       </div>
     </main>
