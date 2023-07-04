853981c7e578dc0db37f17a9d92d4910933855b3
Sma-source <rabhi.smain@gmail.com>
2023-07-04 18:09:14 +0200

    create overlay fro dialogue modal

--- components/CardDetails.tsx
+++ components/CardDetails.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Fragment } from "react";
 import Image from "next/image";
 
@@ -10,7 +11,25 @@ interface CardDetailsProps {
   car: CarProps;
 }
 const CardDetails = ({ isOpen, closeModal, car }: CardDetailsProps) => {
-  return <div>CardDetails</div>;
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
+  );
 };
 
 export default CardDetails;
