ebaa8877917f745b549ac290fb02836684ed0233
Sma-source <rabhi.smain@gmail.com>
2023-07-04 17:53:34 +0200

    create interface

--- components/CardDetails.tsx
+++ components/CardDetails.tsx
@@ -1,6 +1,12 @@
+import { CarProps } from "@/types";
 import React from "react";
 
-const CardDetails = () => {
+interface CardDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
+}
+const CardDetails = ({ isOpen, closeModal, car }: CardDetailsProps) => {
   return <div>CardDetails</div>;
 };
 
