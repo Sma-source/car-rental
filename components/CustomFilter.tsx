0134e816841cd60317e8f714d678d859763f8df5
Sma-source <rabhi.smain@gmail.com>
2023-07-05 22:03:03 +0200

    set filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,12 +4,27 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]); // State for storing the selected option
+
+  // update the URL search parameters and navigate to the new URL
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName, { scroll: false });
+  };
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e); // Update the selected option in state
+          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
+        }}
+      >
         <div className="relative w-fit z-10">
           {/* Button for the listbox */}
           <Listbox.Button className="custom-filter__btn">
