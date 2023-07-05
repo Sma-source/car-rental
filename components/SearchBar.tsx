33f8d4afd9c194052afba70cc63534591ac8c4a0
Sma-source <rabhi.smain@gmail.com>
2023-07-05 20:03:24 +0200

    create searchbar for model

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,9 +2,23 @@
 import { useState } from "react";
 
 import SearchManufacture from "./SearchManufacture";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacture, setManufacture] = useState("");
+  const [model, setModel] = useState("");
   const handleSearch = () => {};
   return (
     <form className="searchbar" onSubmit={handleSearch}>
@@ -13,7 +27,27 @@ const SearchBar = () => {
           manufacture={manufacture}
           setManufacture={setManufacture}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan..."
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
