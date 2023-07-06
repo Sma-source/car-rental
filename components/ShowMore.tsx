0dcc487afd6b4c60e70bee43d283bb6a366005ab
Sma-source <rabhi.smain@gmail.com>
2023-07-06 16:09:39 +0200

    import showmoreprops

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
 
-const ShowMore = () => {
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   return <div>ShowMore</div>;
 };
 
