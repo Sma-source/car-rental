3d78cca1cac0477ddc12cc68a34f9de722f90bfd
Sma-source <rabhi.smain@gmail.com>
2023-07-06 16:12:57 +0200

    add custombutton

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,6 +3,7 @@
 import { ShowMoreProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 import { useRouter } from "next/navigation";
+import CustomButton from "./CustomButton";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
@@ -16,7 +17,18 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
 
     router.push(newPathname);
   };
-  return <div>ShowMore</div>;
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ShowMore;
