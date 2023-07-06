564d5d63a0a0d206d29775ee40b082050cc7562e
Sma-source <rabhi.smain@gmail.com>
2023-07-06 16:11:07 +0200

    create const handleNavigation

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -5,6 +5,16 @@ import { useRouter } from "next/navigation";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
+
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
+
+    // Update the "limit" search parameter in the URL with the new value
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathname);
+  };
   return <div>ShowMore</div>;
 };
 
